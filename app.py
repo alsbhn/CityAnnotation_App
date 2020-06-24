9f90fe91b7726ebeae7617b5ddc0f594e0470375
Local Administrator <LocalAdmin@TUD257387.tudelft.net>
2020-06-24 17:04:23 +0200

    edit

--- app.py
+++ app.py
@@ -3,17 +3,13 @@ import pandas as pd
 import numpy as np
 import altair as alt
 import pydeck as pdk
-import time
+#import time
 from nltk.tokenize import sent_tokenize
 import re
 from newspaper import Article
 from selenium import webdriver
-from functions import pred_percent, pred_sent, pred_array
+#from functions import pred_percent, pred_sent, pred_array
 
-DATE_TIME = "date/time"
-DATA_URL = (
-    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
-)
 
 st.title("Annotation App")
 st.markdown(
@@ -80,7 +76,7 @@ text = re.sub(r"[^A-Za-z0-9^,.'-]", " ", text)
 text = re.sub(' +',' ', text)
 for i, sent in enumerate (sent_tokenize(text)):
     st.write(sent)
-    st.write(pred_sent(sent))
+    #st.write(pred_sent(sent))
     st.multiselect(f"sent {i+1}",('subsistence','protection','affection','participation'))
 
 
