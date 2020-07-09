949e5818d086895e131101fc88052997ba752b25
Local Administrator <LocalAdmin@TUD257387.tudelft.net>
2020-07-09 21:22:47 +0200

    asd

--- app.py
+++ app.py
@@ -14,15 +14,15 @@ import ast
 from selenium import webdriver
 import os
 
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+#GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+#CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
-chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
+chrome_options.binary_location = GOOGLE_CHROME_BIN
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = GOOGLE_CHROME_PATH
-
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+
 
 from sqlalchemy import create_engine
 from sqlalchemy.sql import select, update
