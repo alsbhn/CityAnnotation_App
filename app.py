f39530fc0675ddd120aded9ab324af2cb3c8e484
Local Administrator <LocalAdmin@TUD257387.tudelft.net>
2020-07-10 10:17:08 +0200

    ajjdf

--- app.py
+++ app.py
@@ -14,14 +14,6 @@ import ast
 from selenium import webdriver
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.sql import select, update
 from sqlalchemy import MetaData, Table,Column, Integer, String
@@ -140,10 +132,20 @@ st.subheader(data['title'])
 
 ####### SCREEN SHOT ########
 if st.button('Screenshot'):
-    driver.get(data['url'])
-    screenshot = driver.save_screenshot('my_screenshot.png')
-    driver.quit()
-    st.image('my_screenshot.png',width=700)
+    try:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+        driver.get(data['url'])
+        screenshot = driver.save_screenshot('my_screenshot.png')
+        driver.quit()
+        st.image('my_screenshot.png',width=700)
+    except:
+        st.write('Sorry! Cannot Load the Screenshot.')
 
 # City
 st.markdown(data['city'])
